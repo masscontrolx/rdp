c2a82038578bf467e32f5df9066f00b7629574b1
masscontrolx <38991956+masscontrolx@users.noreply.github.com>
2021-02-27 11:02:44 +0300

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -30,6 +30,6 @@ printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+su - mass -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 5545 \n'
